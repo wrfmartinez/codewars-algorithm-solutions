34372c161c058fa16a135013759142a6754931b4
wrfmartinez <wrmartinez709@gmail.com>
2024-04-15 08:41:32 -0400

    Write pseudocode to arrayDiff function

---
+++ js/arrayDiff.js
@@ -0,0 +1,10 @@
+function arrayDiff(a, b) {
+  // This function takes two arrays for parameters
+  // Remove all occurences of values from array a that match the values in array b
+  // Keepiing their order
+
+  // 1. Iterate through array b
+  // 2. Iterate through array a
+  // 3. Check if each value in array a matches the values in array b
+  // 4. Remove that value from array a if it does match using .pop()
+}
