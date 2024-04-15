e9df675ecc46485cb09fe1cb8c0d6d61bd36e5a8
wrfmartinez <wrmartinez709@gmail.com>
2024-04-15 09:37:28 -0400

    Adjust pseudocode and solve the algorithm

--- js/arrayDiff.js
+++ js/arrayDiff.js
@@ -6,5 +6,17 @@ function arrayDiff(a, b) {
   // 1. Iterate through array b
   // 2. Iterate through array a
   // 3. Check if each value in array a matches the values in array b
-  // 4. Remove that value from array a if it does match using .pop()
+  // 4. Remove that value from array a if it does match using .splice() since .pop() does not remove a value at the current index but rather the value at the end of an array
+
+  for (let i = 0; i < b.length; i++) {
+    for (let j = a.length - 1; j >= 0; j--) {
+      if (b[i] === a[j]) {
+        a.splice(j, 1);
+      }
+    }
+  }
+
+  return a;
 }
+
+console.log(arrayDiff([1, 2, 2, 2, 3],[2]));
